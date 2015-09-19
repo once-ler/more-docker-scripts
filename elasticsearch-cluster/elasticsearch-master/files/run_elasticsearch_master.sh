c558d274cf04bbea40512c4f52d4b542241ce1ee
htaox <htaox@hotmail.com>
2015-09-19 16:35:52 -0400

    Lower java version

--- elasticsearch-cluster/elasticsearch-master/files/run_elasticsearch_master.sh
+++ elasticsearch-cluster/elasticsearch-master/files/run_elasticsearch_master.sh
@@ -14,7 +14,7 @@ sed -i "s/@MASTER@/true/g" $ES_HOME/conf/elasticsearch.yml
 sed -i "s/@DATA@/false/g" $ES_HOME/conf/elasticsearch.yml
 
 #elasticsearch requires hostname loopback
-sudo mungehosts -a "127.0.0.1  ${HOSTNAME}"
+sudo mungehosts -l $HOSTNAME
 cat /etc/hosts
 
 ES_HEAP_SIZE=1g
--- elasticsearch-cluster/elasticsearch-worker/files/run_elasticsearch_worker.sh
+++ elasticsearch-cluster/elasticsearch-worker/files/run_elasticsearch_worker.sh
@@ -14,7 +14,7 @@ sed -i "s/@MASTER@/false/g" $ES_HOME/conf/elasticsearch.yml
 sed -i "s/@DATA@/true/g" $ES_HOME/conf/elasticsearch.yml
 
 #elasticsearch requires hostname loopback
-sudo mungehosts -a "127.0.0.1  ${HOSTNAME}"
+sudo mungehosts -l $HOSTNAME
 cat /etc/hosts
 
 ES_HEAP_SIZE=2g
