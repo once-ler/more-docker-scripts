cd98172c362d94a0831dffd221c79812d7097538
htaox <htaox@hotmail.com>
2015-09-20 07:12:55 -0400

    Change conf to config

--- elasticsearch-cluster/elasticsearch-master/files/run_elasticsearch_master.sh
+++ elasticsearch-cluster/elasticsearch-master/files/run_elasticsearch_master.sh
@@ -7,16 +7,16 @@ echo 'Starting Elasticsearch Master'
 IP=$(ip -o -4 addr list eth0 | perl -n -e 'if (m{inet\s([\d\.]+)\/\d+\s}xms) { print $1 }')
 echo "MASTER_IP=$IP"
 
-sed -i "s/@IP@/$IP/g" $ES_HOME/conf/elasticsearch.yml
-# sed -i "s|^network.host:.*|network.host: $IP|" $ES_HOME/conf/elasticsearch.yml
+sed -i "s/@IP@/$IP/g" $ES_HOME/config/elasticsearch.yml
+# sed -i "s|^network.host:.*|network.host: $IP|" $ES_HOME/config/elasticsearch.yml
 
-sed -i "s/@MASTER@/true/g" $ES_HOME/conf/elasticsearch.yml
-sed -i "s/@DATA@/false/g" $ES_HOME/conf/elasticsearch.yml
+sed -i "s/@MASTER@/true/g" $ES_HOME/config/elasticsearch.yml
+sed -i "s/@DATA@/false/g" $ES_HOME/config/elasticsearch.yml
 
 #elasticsearch requires hostname loopback
-sudo mungehosts -l $HOSTNAME
-cat /etc/hosts
+#sudo mungehosts -l $HOSTNAME
+#cat /etc/hosts
 
 ES_HEAP_SIZE=1g
 
-sudo -u elasticsearch $ES_HOME/bin/elasticsearch -f -Des.config=$ES_HOME/conf/elasticsearch.yml -Xms$ES_HEAP_SIZE -Xmx$ES_HEAP_SIZE
+sudo -u elasticsearch $ES_HOME/bin/elasticsearch -f -Des.config=$ES_HOME/config/elasticsearch.yml -Xms$ES_HEAP_SIZE -Xmx$ES_HEAP_SIZE
--- elasticsearch-cluster/elasticsearch-worker/files/run_elasticsearch_worker.sh
+++ elasticsearch-cluster/elasticsearch-worker/files/run_elasticsearch_worker.sh
@@ -7,16 +7,16 @@ echo 'Starting Elasticsearch Worker'
 IP=$(ip -o -4 addr list eth0 | perl -n -e 'if (m{inet\s([\d\.]+)\/\d+\s}xms) { print $1 }')
 echo "WORKER_IP=$IP"
 
-sed -i "s/@IP@/$IP/g" $ES_HOME/conf/elasticsearch.yml
-#sed -i "s|^network.host:.*|network.host: $IP|" $ES_HOME/conf/elasticsearch.yml
+sed -i "s/@IP@/$IP/g" $ES_HOME/config/elasticsearch.yml
+#sed -i "s|^network.host:.*|network.host: $IP|" $ES_HOME/config/elasticsearch.yml
 
-sed -i "s/@MASTER@/false/g" $ES_HOME/conf/elasticsearch.yml
-sed -i "s/@DATA@/true/g" $ES_HOME/conf/elasticsearch.yml
+sed -i "s/@MASTER@/false/g" $ES_HOME/config/elasticsearch.yml
+sed -i "s/@DATA@/true/g" $ES_HOME/config/elasticsearch.yml
 
 #elasticsearch requires hostname loopback
-sudo mungehosts -l $HOSTNAME
-cat /etc/hosts
+#sudo mungehosts -l $HOSTNAME
+#cat /etc/hosts
 
 ES_HEAP_SIZE=2g
 
-sudo -u elasticsearch $ES_HOME/bin/elasticsearch -f -Des.config=$ES_HOME/conf/elasticsearch.yml -Xms$ES_HEAP_SIZE -Xmx$ES_HEAP_SIZE
+sudo -u elasticsearch $ES_HOME/bin/elasticsearch -f -Des.config=$ES_HOME/config/elasticsearch.yml -Xms$ES_HEAP_SIZE -Xmx$ES_HEAP_SIZE
